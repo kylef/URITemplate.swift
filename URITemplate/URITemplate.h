b8fafc9b6a3ff578fe9f74746609f4ab932638d1
Kyle Fuller <inbox@kylefuller.co.uk>
2014-11-25 22:59:26 +0100

    Initial Xcode project

---
+++ URITemplate/URITemplate.h
@@ -0,0 +1,19 @@
+//
+//  URITemplate.h
+//  URITemplate
+//
+//  Created by Kyle Fuller on 25/11/2014.
+//  Copyright (c) 2014 Kyle Fuller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for URITemplate.
+FOUNDATION_EXPORT double URITemplateVersionNumber;
+
+//! Project version string for URITemplate.
+FOUNDATION_EXPORT const unsigned char URITemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URITemplate/PublicHeader.h>
+
+
