2cf588d6e2899888fbcb0cd3825a712ed0d5b50c
Kyle Fuller <inbox@kylefuller.co.uk>
2014-11-26 01:46:18 +0100

    [Tests] Break out variable tests to separate file

---
+++ URITemplateTests/URITemplateVariablesTests.swift
@@ -0,0 +1,60 @@
+//
+//  URITemplateVariablesTests.swift
+//  URITemplate
+//
+//  Created by Kyle Fuller on 26/11/2014.
+//  Copyright (c) 2014 Kyle Fuller. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import URITemplate
+
+// MARK: Variables
+
+class URITemplateVariablesTests : XCTestCase {
+  func testVariables() {
+    let template = URITemplate(template:"{scheme}://{hostname}/")
+    XCTAssertEqual(template.variables(), ["scheme", "hostname"])
+  }
+
+  func testMultipleVariablesInExpression() {
+    let template = URITemplate(template:"test/{a,b}")
+    XCTAssertEqual(template.variables(), ["a", "b"])
+  }
+
+  func testReservedVariablesInExpression() {
+    let template = URITemplate(template:"test/{+reserved}")
+    XCTAssertEqual(template.variables(), ["reserved"])
+  }
+
+  func testLabelVariablesInExpression() {
+    let template = URITemplate(template:"test/{.label}")
+    XCTAssertEqual(template.variables(), ["label"])
+  }
+
+  func testFragmentVariablesInExpression() {
+    let template = URITemplate(template:"test/{#fragment}")
+    XCTAssertEqual(template.variables(), ["fragment"])
+  }
+
+  func testPathSegmentVariablesInExpression() {
+    let template = URITemplate(template:"test/{/segment}")
+    XCTAssertEqual(template.variables(), ["segment"])
+  }
+
+  func testPathParameterVariablesInExpression() {
+    let template = URITemplate(template:"test/{;parameter}")
+    XCTAssertEqual(template.variables(), ["parameter"])
+  }
+
+  func testFormStyleQueryVariablesInExpression() {
+    let template = URITemplate(template:"test/{?query}")
+    XCTAssertEqual(template.variables(), ["query"])
+  }
+
+  func testFormStyleQueryContinuationVariablesInExpression() {
+    let template = URITemplate(template:"test/{&continuation}")
+    XCTAssertEqual(template.variables(), ["continuation"])
+  }
+}
