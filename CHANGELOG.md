bbe741367cbb1b152346504b9335d635ba63ebae
Mark Kieling <mkieling@jawbone.com>
2017-08-14 11:16:15 -0700

    feat: Add Swift 4 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog for URITemplate
 
+## Master
+
+### Enhancements
+
+- Adds support for Swift 4.0.
+
 ## 2.0.1
 
 Internal refactoring to prevent use of deprecated system APIs.
--- Sources/URITemplate.swift
+++ Sources/URITemplate.swift
@@ -210,11 +210,11 @@ public struct URITemplate : CustomStringConvertible, Equatable, Hashable, Expres
         var extractedVariables:[String: String] = [:]
 
         for (index, variable) in variables.enumerated() {
-			#if swift(>=4.0)
-				let range = result.range(at: index + 1)
-			#else
-				let range = result.rangeAt(index + 1)
-			#endif
+#if swift(>=4.0)
+          let range = result.range(at: index + 1)
+#else
+          let range = result.rangeAt(index + 1)
+#endif
           let value = NSString(string: input.substring(with: range)).removingPercentEncoding
           extractedVariables[variable] = value
         }
