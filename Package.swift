81fc74901869a98dfef0b5845badfebf390b9194
Kyle Fuller <kyle@fuller.li>
2019-06-06 21:45:14 +0100

    style: fix syntax for : string

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ let package = Package(
     .library(name: "URITemplate", targets: ["URITemplate"])
   ],
   dependencies: [
-    .package(url:"https://github.com/kylef/PathKit.git", .upToNextMinor(from:"1.0.0")),
-    .package(url:"https://github.com/kylef/Spectre.git", .upToNextMinor(from:"0.9.0"))
+    .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "1.0.0")),
+    .package(url: "https://github.com/kylef/Spectre.git", .upToNextMinor(from: "0.9.0"))
   ],
   targets: [
     .target(name: "URITemplate", dependencies: [], path: "Sources"),
-    .testTarget(name: "URITemplateTests", dependencies: ["URITemplate", "PathKit", "Spectre"], path:"Tests/URITemplateTests")
+    .testTarget(name: "URITemplateTests", dependencies: ["URITemplate", "PathKit", "Spectre"], path: "Tests/URITemplateTests")
   ]
 )
